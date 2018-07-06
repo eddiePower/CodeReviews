0ec04483d17ec373eeb42ab6a49d93355c3084da
eddiePower <eddie.power@icloud.com>
2018-07-07 03:10:57 +1000

    Fix up namespace issues after move to folders for mVVm

--- jeylabsCodeReviews/Views/MainWindow.xaml.cs
+++ jeylabsCodeReviews/Views/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
+﻿using System.Windows;
 using System.Windows.Shapes;
 
-namespace jeylabsCodeReviews
+namespace jeylabsCodeReviews.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -31,7 +19,7 @@ namespace jeylabsCodeReviews
 
             Shape funnyStuff = pageViewModel.DrawShape;
 
-            shapeDrawingCanvas.Children.Add(funnyStuff);
+            ShapeDrawingCanvas.Children.Add(funnyStuff);
         }
     }
 }
